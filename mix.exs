04fe373fdd902e45416a1dceb5fd54b39a150bae
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-11-19 18:19:52 +0900

    Add :ex_doc to mix.exs

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule ExtrText.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.25.0", only: :dev, runtime: false},
       {:saxy, "~> 1.4"}
     ]
   end
