cca81b88db33eb51d720af57e9b9d9c3eaf4c419
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-11-19 18:05:14 +0900

    Create a mix project skeleton

---
+++ lib/extr_text.ex
@@ -0,0 +1,18 @@
+defmodule ExtrText do
+  @moduledoc """
+  Documentation for `ExtrText`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExtrText.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/extr_text_test.exs
@@ -0,0 +1,8 @@
+defmodule ExtrTextTest do
+  use ExUnit.Case
+  doctest ExtrText
+
+  test "greets the world" do
+    assert ExtrText.hello() == :world
+  end
+end
