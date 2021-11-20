7bb73d437877f1bb940f3ec9c721b586eaaadf4c
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-11-20 09:48:05 +0900

    Extract text from xlsx file

--- lib/extr_text.ex
+++ lib/extr_text.ex
@@ -7,24 +7,57 @@ defmodule ExtrText do
   @doc """
   Extract title, subject, description and body as a joined string from the specified binary data.
   """
-  @spec extract(binary()) :: {:ok, String.t()}
+  @spec extract(binary()) :: {:ok, String.t()} | {:error, String.t()}
   def extract(data) do
     tmpdir = System.tmp_dir!()
-    {:ok, _paths} = :zip.unzip(data, cwd: tmpdir)
+    now = DateTime.utc_now()
+    {usec, _} = now.microsecond
+    subdir = tmpdir <> "/extr-text-" <> Integer.to_string(usec)
+
+    case File.mkdir_p(subdir) do
+      :ok -> do_extract(data, subdir)
+      {:error, _reason} -> {:error, "Can't create #{subdir}."}
+    end
+  end
+
+  defp do_extract(data, subdir) do
+    {:ok, paths} = :zip.unzip(data, cwd: subdir)
+
+    type =
+      cond do
+        Enum.any?(paths, fn path -> path == subdir <> "/word/document.xml" end) -> :docx
+        Enum.any?(paths, fn path -> path == subdir <> "/xl/sharedStrings.xml" end) -> :xsls
+        true -> :unknown
+      end
 
     attributes =
-      case File.read(Path.join(tmpdir, "docProps/core.xml")) do
+      case File.read(Path.join(subdir, "docProps/core.xml")) do
         {:ok, xml} -> extract_attributes(xml)
         {:error, _} -> nil
       end
 
-    document =
-      case File.read(Path.join(tmpdir, "word/document.xml")) do
-        {:ok, xml} -> extract_text(xml)
-        {:error, _} -> nil
+    {handler, filename} =
+      case type do
+        :docs -> {WordDocumentHandler, "word/document.xml"}
+        :xsls -> {ExcelSharedStringsHandler, "xl/sharedStrings.xml"}
+        :unknown -> {nil, nil}
       end
 
-    {:ok, attributes <> "\n" <> document}
+    if handler && filename do
+      document =
+        case File.read(Path.join(subdir, filename)) do
+          {:ok, xml} -> extract_text(handler, xml)
+          {:error, _} -> nil
+        end
+
+      if attributes && document do
+        {:ok, attributes <> "\n" <> document}
+      else
+        {:error, "Could not parse XML files."}
+      end
+    else
+      {:error, "Could not find a target XML file."}
+    end
   end
 
   defp extract_attributes(xml) do
@@ -32,8 +65,8 @@ defmodule ExtrText do
     reverse_and_join(texts)
   end
 
-  defp extract_text(xml) do
-    {:ok, texts} = Saxy.parse_string(xml, WordDocumentHandler, [])
+  defp extract_text(handler, xml) do
+    {:ok, texts} = Saxy.parse_string(xml, handler, [])
     reverse_and_join(texts)
   end
 
--- test/extr_text_test.exs
+++ test/extr_text_test.exs
@@ -9,5 +9,11 @@ defmodule ExtrTextTest do
       {:ok, text} = ExtrText.extract(docx)
       assert text == "Elixir\nTest\nGreeting\nGreeting\nHello, world!"
     end
+
+    test "extract text from a .xlsx file" do
+      xlsx = File.read!(Path.join(@files_dir, "prefectures.xlsx"))
+      {:ok, text} = ExtrText.extract(xlsx)
+      assert text == "Excel\nElixir\nNumbers\nPrefectures\nTokyo\nOsaka\nNagoya"
+    end
   end
 end
