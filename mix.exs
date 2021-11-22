c3cb81ec6d9e06ee98329b2725eeb0e8f5b75bf6
Tsutomu Kuroda <t-kuroda@oiax.jp>
2021-11-23 02:02:41 +0900

    Bump versiont to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExtrText.MixProject do
   def project do
     [
       app: :extr_text,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: "An Elixir library for extracting text from docs/xlsx/pptx files.",
